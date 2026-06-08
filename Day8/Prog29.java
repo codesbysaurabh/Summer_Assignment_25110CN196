796e1c52e96a85a7799dd0c013477c5f9a5540c7
codesbysaurabh <saurabhsingh4510@gmail.com>
2026-06-08 20:01:52 +0530

    Day8

---
+++ Day8/Prog29.java
@@ -0,0 +1,12 @@
+package Day8;
+
+public class Prog29 {
+    public static void main(String arg[]){
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
