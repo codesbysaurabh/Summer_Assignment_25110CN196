9461cef3cce12534d999f7145b527f07d9f6a115
codesbysaurabh <saurabhsingh4510@gmail.com>
2026-06-14 22:50:21 +0530

    Day14

---
+++ Day14/Prog53.java
@@ -0,0 +1,25 @@
+package Day14;
+
+import java.util.Scanner;
+
+public class Prog53 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter array elements:");
+        for(int i = 0; i < n; i++) arr[i] = sc.nextInt();
+
+        System.out.print("Enter element to search: ");
+        int target = sc.nextInt();
+
+        for(int i = 0; i < n; i++)
+            if(arr[i] == target) {
+                System.out.println("Element found at index " + i);
+                break;
+            }
+    }
+}
