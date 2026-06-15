a7bdc348c1e8b495a14f661f62a42f88d20cefb5
codesbysaurabh <saurabhsingh4510@gmail.com>
2026-06-15 19:25:43 +0530

    Day15

---
+++ Day15/Prog60.java
@@ -0,0 +1,33 @@
+package Day15;
+
+import java.util.Scanner;
+
+public class Prog60 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+        for(int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        int j = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(arr[i] != 0) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j++;
+            }
+        }
+
+        System.out.println("Array after moving zeroes to end:");
+        for(int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
+    }
+}
