923099eac929964f374c26828ddeb20ba113b9a7
codesbysaurabh <saurabhsingh4510@gmail.com>
2026-06-23 13:00:18 +0530

    Day23

---
+++ Day23/Prog92.java
@@ -0,0 +1,30 @@
+package Day23;
+
+import java.util.Scanner;
+
+public class Prog92 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        int[] freq = new int[256];
+
+        for (int i = 0; i < str.length(); i++) {
+            freq[str.charAt(i)]++;
+        }
+
+        int max = 0;
+        char ch = ' ';
+
+        for (int i = 0; i < 256; i++) {
+            if (freq[i] > max) {
+                max = freq[i];
+                ch = (char) i;
+            }
+        }
+
+        System.out.println("Maximum occurring character: " + ch);
+    }
+}
